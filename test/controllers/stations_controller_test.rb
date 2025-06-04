1e7a2eac12a106a79780aefdbe1e0e5ffbdc976c
huutai09072003 <tain6507@gmail.com>
2025-06-04 14:59:00 +0700

    Test

--- test/controllers/stations_controller_test.rb
+++ test/controllers/stations_controller_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: stations
+#
+#  id              :bigint           not null, primary key
+#  location        :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require "test_helper"
 
 class StationsControllerTest < ActionDispatch::IntegrationTest
