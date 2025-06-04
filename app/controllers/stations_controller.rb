d9fd5cd3ece95c4ec9aafb8b2ffb1c59e360aefc
huutai09072003 <tain6507@gmail.com>
2025-06-04 16:36:46 +0700

    update

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,5 +1,5 @@
 class StationsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def login
     station = Station.find_by(name: params[:name])
